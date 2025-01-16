cc2cc20ab7d5bebaa623315adb5e02ae6b69a504
rgsk <guptarahul70322@gmail.com>
2025-01-16 13:38:57 +0530

    working

--- src/components/Sample/SampleImageCombiner.tsx
+++ src/components/Sample/SampleImageCombiner.tsx
@@ -29,75 +29,122 @@ const SampleImageCombiner = () => {
       return;
     }
 
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
+    const drawImage = (textHeight?: number) => {
+      console.log({ textHeight });
 
-    const profileImg = new Image();
-    const backgroundImg = new Image();
+      const canvas = canvasRef.current;
+      if (!canvas) {
+        return;
+      }
+      const ctx = canvas.getContext("2d");
+      if (!ctx) return;
 
-    backgroundImg.src = URL.createObjectURL(backgroundImage);
-    backgroundImg.onload = () => {
-      // Set canvas dimensions to match background image resolution
-      const dpr = window.devicePixelRatio || 1; // Device Pixel Ratio
-      canvas.width = backgroundImg.width * dpr;
-      canvas.height = backgroundImg.height * dpr;
-      ctx.scale(dpr, dpr); // Scale the canvas for high-resolution rendering
+      const profileImg = new Image();
+      const backgroundImg = new Image();
 
-      // Draw background image
-      ctx.drawImage(
-        backgroundImg,
-        0,
-        0,
-        backgroundImg.width,
-        backgroundImg.height
-      );
-
-      // Load the profile image
-      profileImg.src = URL.createObjectURL(profileImage);
-      profileImg.onload = () => {
-        // Draw profile image inside the designated box
-        // const profileBox = { top: 125, left: 117, width: 400, height: 400 };
-        const profileBox = { top: 75, left: 70, width: 238, height: 238 };
+      backgroundImg.src = URL.createObjectURL(backgroundImage);
+      backgroundImg.onload = () => {
+        canvas.width = backgroundImg.width;
+        canvas.height = backgroundImg.height;
+        const multiplier = 1;
+        // Draw background image
         ctx.drawImage(
-          profileImg,
-          profileBox.left,
-          profileBox.top,
-          profileBox.width,
-          profileBox.height
+          backgroundImg,
+          0,
+          0,
+          backgroundImg.width,
+          backgroundImg.height
         );
 
-        const textHeight = 115;
+        // Load the profile image
+        profileImg.src = URL.createObjectURL(profileImage);
 
-        // Add text inside the second box
-        ctx.font = `bold 30px sans-serif`;
-        ctx.fillStyle = "white";
-        ctx.textAlign = "center";
+        profileImg.onload = () => {
+          // Draw profile image inside the designated box
+          const profileBox = { top: 125, left: 117, width: 400, height: 400 };
+          // const profileBox = { top: 75, left: 70, width: 238, height: 238 };
+          profileBox.top *= multiplier;
+          profileBox.left *= multiplier;
+          profileBox.height *= multiplier;
+          profileBox.width *= multiplier;
+          ctx.drawImage(
+            profileImg,
+            profileBox.left,
+            profileBox.top,
+            profileBox.width,
+            profileBox.height
+          );
 
-        // const textBox = {
-        //   top: 553,
-        //   left: 104,
-        //   width: 426,
-        //   height: 193,
-        // };
-        const textBox = {
-          top: 330,
-          left: 62,
-          width: 254,
-          height: 115,
+          // Add text inside the second box
+          const primaryFontSize = 30;
+          const secondaryFontSize = 20;
+          const primaryTextMargin = 10;
+          const secondaryTextMargin = 10;
+          ctx.font = `bold ${primaryFontSize}px sans-serif`;
+          ctx.fillStyle = "white";
+          ctx.textAlign = "center";
+          ctx.textBaseline = "top";
+
+          const textBox = {
+            top: 553,
+            left: 104,
+            width: 426,
+            height: 193,
+          };
+          // const textBox = {
+          //   top: 330,
+          //   left: 62,
+          //   width: 254,
+          //   height: 115,
+          // };
+          textBox.top *= multiplier;
+          textBox.left *= multiplier;
+          textBox.height *= multiplier;
+          textBox.width *= multiplier;
+
+          const leftOffset = textBox.left + textBox.width / 2;
+          const topOffset =
+            textBox.top + textBox.height / 2 - (textHeight ?? 0) / 2;
+          ctx.fillText(name, leftOffset, topOffset);
+          const metrics = ctx.measureText(name);
+          const primaryTextHeight =
+            metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+          ctx.font = `${secondaryFontSize}px sans-serif`;
+          ctx.fillText(
+            designation,
+            leftOffset,
+            topOffset + primaryTextHeight + primaryTextMargin
+          );
+          const metrics2 = ctx.measureText(designation);
+          const secondaryTextHeight =
+            metrics2.actualBoundingBoxAscent +
+            metrics2.actualBoundingBoxDescent;
+          ctx.fillText(
+            company,
+            leftOffset,
+            topOffset +
+              primaryTextHeight +
+              primaryTextMargin +
+              secondaryTextHeight +
+              secondaryTextMargin
+          );
+          if (textHeight) {
+            // Set the preview source
+            const outputImage = canvas.toDataURL("image/png");
+            setPreviewSrc(outputImage);
+          } else {
+            textHeight =
+              primaryTextHeight +
+              primaryTextMargin +
+              secondaryTextHeight +
+              secondaryTextMargin +
+              secondaryTextHeight;
+            drawImage(textHeight);
+          }
         };
-        const leftOffset = textBox.left + textBox.width / 2;
-        const topOffset = textBox.top;
-        ctx.fillText(name, leftOffset, topOffset);
-        ctx.font = "20px sans-serif";
-        ctx.fillText(designation, leftOffset, topOffset + 30);
-        ctx.fillText(company, leftOffset, topOffset + 60);
-
-        // Set the preview source
-        const outputImage = canvas.toDataURL("image/png");
-        setPreviewSrc(outputImage);
       };
     };
+    drawImage();
   };
 
   return (
