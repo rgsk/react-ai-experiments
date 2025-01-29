01927a9ecc7c39725062cf62b951d940f1fe2acf
rgsk <guptarahul70322@gmail.com>
2025-01-29 11:19:54 +0530

    monaco editor integration

--- src/components/ChatPage/Children/SyntaxHighlighter.tsx
+++ src/components/ChatPage/Children/SyntaxHighlighter.tsx
@@ -1,10 +1,9 @@
+import Editor from "@monaco-editor/react";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
-import { Prism as ReactSyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import TextareaAutosize from "react-textarea-autosize";
 import { LoadingSpinner } from "~/components/Shared/LoadingSpinner";
 import useCopyToClipboard from "~/hooks/useCopyToClipboard";
+
 import { cn } from "~/lib/utils";
 import experimentsService from "~/services/experimentsService";
 interface SyntaxHighlighterProps {
@@ -19,7 +18,6 @@ const SyntaxHighlighter: React.FC<SyntaxHighlighterProps> = ({
   isCodeOutput,
   codeProps,
 }) => {
-  const [editModeActive, setEditModeActive] = useState(false);
   const [code, setCode] = useState(initialCode);
   const { copied, copy } = useCopyToClipboard();
   const executeCodeMutationResult = useMutation({
@@ -36,136 +34,83 @@ const SyntaxHighlighter: React.FC<SyntaxHighlighterProps> = ({
       language,
     });
   };
+
+  const countOfLines = code.split("\n").length;
+  const lineHeight = 18;
+  const paddingTop = 20;
+  const paddingBottom = 20;
   return (
-    <div className="relative">
-      <div
-        className={cn(
-          "absolute top-0 right-0 bg-[#50505a] w-full",
-          "flex justify-between items-center",
-          "rounded-tl-[16px] rounded-tr-[16px]",
-          "px-[16px] py-[8px]"
-        )}
-      >
-        {language === "default" ? (
-          <span></span>
-        ) : (
-          <span className="text-white text-xs">{language}</span>
-        )}
-        <div className="flex gap-3">
-          <button
-            className="text-white text-xs border border-w rounded-md px-2 pt-[3px] pb-[1px]"
-            onClick={() => {
-              copy(code);
-            }}
-          >
-            {copied ? "Copied!" : "Copy"}
-          </button>
-          {!isCodeOutput && (
-            <>
-              <button
-                className="text-white text-xs border border-w rounded-md px-2 pt-[3px] pb-[1px]"
-                onClick={executeCode}
-              >
-                Run Code
-              </button>
-              {editModeActive ? (
-                <>
-                  <button
-                    className="text-white text-xs border border-w rounded-md px-2 pt-[3px] pb-[1px]"
-                    onClick={() => {
-                      setEditModeActive(false);
-                    }}
-                  >
-                    Exit Edit
-                  </button>
-                </>
-              ) : (
+    <div>
+      <div className="rounded-[12px] overflow-hidden">
+        <div
+          className={cn(
+            "bg-[#50505a] w-full",
+            "flex justify-between items-center",
+            "px-[16px] py-[8px]"
+          )}
+        >
+          {language === "default" ? (
+            <span></span>
+          ) : (
+            <span className="text-white text-xs">{language}</span>
+          )}
+          <div className="flex gap-3">
+            <button
+              className="text-white text-xs border border-w rounded-md px-2 pt-[3px] pb-[1px]"
+              onClick={() => {
+                copy(code);
+              }}
+            >
+              {copied ? "Copied!" : "Copy"}
+            </button>
+            {!isCodeOutput && (
+              <>
                 <button
                   className="text-white text-xs border border-w rounded-md px-2 pt-[3px] pb-[1px]"
-                  onClick={() => {
-                    setEditModeActive(true);
-                  }}
+                  onClick={executeCode}
                 >
-                  Edit
+                  Run Code
                 </button>
-              )}
-            </>
-          )}
+              </>
+            )}
+          </div>
         </div>
+
+        <Editor
+          defaultLanguage={language}
+          defaultValue={code}
+          theme="vs-dark"
+          height={countOfLines * lineHeight + paddingBottom}
+          options={{
+            fontSize: 12,
+            lineHeight: lineHeight,
+            scrollBeyondLastLine: false,
+            lineNumbers: "off",
+            minimap: {
+              enabled: false, // Disable the minimap
+            },
+            padding: {
+              top: paddingTop,
+            },
+          }}
+          onChange={(newValue) => setCode(newValue || "")}
+        />
       </div>
-
-      {editModeActive ? (
-        <TextareaAutosize
-          className="w-full"
-          style={{
-            borderRadius: "16px",
-            padding: "16px",
-            paddingTop: "50px",
-            outline: "1px solid black",
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              if (e.shiftKey) {
-                e.preventDefault();
-                executeCode();
-              }
-            } else if (e.key === "Tab") {
-              e.preventDefault();
-
-              const start = e.currentTarget.selectionStart;
-              const end = e.currentTarget.selectionEnd;
-
-              // Insert 2 spaces or a tab character ("\t")
-              const newCode =
-                code.substring(0, start) + "\t" + code.substring(end);
-
-              setCode(newCode);
-
-              // Move cursor after inserted spaces
-              setTimeout(() => {
-                e.currentTarget.selectionStart = e.currentTarget.selectionEnd =
-                  start + 1;
-              }, 0);
-            }
-          }}
-          value={code}
-          onChange={(e) => {
-            setCode(e.target.value);
-          }}
-        ></TextareaAutosize>
-      ) : (
-        <>
-          {/* @ts-ignore */}
-          <ReactSyntaxHighlighter
-            style={vscDarkPlus}
-            PreTag="div"
-            language={language}
-            {...codeProps}
-            customStyle={{
-              borderRadius: "16px",
-              padding: "16px",
-              paddingTop: "50px",
-            }}
-          >
-            {code}
-          </ReactSyntaxHighlighter>
-        </>
-      )}
-      {executeCodeMutationResult.isPending && (
-        <div>
-          <LoadingSpinner />
-        </div>
-      )}
-      {executeCodeMutationResult.data?.output && (
-        <div>
+      <div className="mt-[20px]">
+        {executeCodeMutationResult.isPending && (
+          <div>
+            <LoadingSpinner />
+          </div>
+        )}
+        {executeCodeMutationResult.data?.output && (
           <SyntaxHighlighter
             code={executeCodeMutationResult.data?.output}
             language={language}
             codeProps={codeProps}
             isCodeOutput={true}
           />
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
