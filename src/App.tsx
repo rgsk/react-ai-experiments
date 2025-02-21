cf0738126e24aa9acbb539162c8ad3ec435fd763
rgsk <guptarahul70322@gmail.com>
2025-02-21 13:10:31 +0530

    log env vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom";
+import environmentVars from "./lib/environmentVars";
 
 function App() {
+  console.log({ environmentVars });
   return (
     <div>
       <Outlet />
