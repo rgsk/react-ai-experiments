9d77100a6deb9026e89ab0c87bd9e80ac9d63ba8
rgsk <guptarahul70322@gmail.com>
2025-02-21 15:00:22 +0530

    fix

--- src/components/PracticePage/PracticePage.tsx
+++ src/components/PracticePage/PracticePage.tsx
@@ -1,19 +1,5 @@
-import { Skeleton } from "../ui/skeleton";
-
 interface PracticePageProps {}
 const PracticePage: React.FC<PracticePageProps> = ({}) => {
-  return (
-    <div className="p-[100px]">
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
-      </div>
-      <div className="space-y-2">
-        <Skeleton className="rounded-full h-[20px] w-1/2" />
-        <Skeleton className="rounded-full h-[20px]" />
-        <Skeleton className="rounded-full h-[20px]" />
-      </div>
-    </div>
-  );
+  return <div className="p-[100px]">Practice page</div>;
 };
 export default PracticePage;
