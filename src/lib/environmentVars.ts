6c9ee2b8be262a4294d688ef5d9b1f8b00704a93
rgsk <guptarahul70322@gmail.com>
2025-02-22 23:16:12 +0530

    fix

--- src/lib/environmentVars.ts
+++ src/lib/environmentVars.ts
@@ -2,6 +2,8 @@ export type AppEnvironment = "development" | "staging" | "production";
 
 const environmentVars = {
   NODE_EXPERIMENTS_SERVER_URL: import.meta.env.VITE_NODE_EXPERIMENTS_SERVER_URL,
+  PYTHON_EXPERIMENTS_SERVER_URL: import.meta.env
+    .VITE_PYTHON_EXPERIMENTS_SERVER_URL,
   APP_ENV: import.meta.env.VITE_APP_ENV as AppEnvironment,
 };
 export default environmentVars;
--- src/services/aiService.ts
+++ src/services/aiService.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import environmentVars from "~/lib/environmentVars";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: environmentVars.PYTHON_EXPERIMENTS_SERVER_URL,
 });
 
 const aiService = {
