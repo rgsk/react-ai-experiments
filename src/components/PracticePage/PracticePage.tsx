8e8a74db1f864949919743094477c4ab2af7ec5b
rgsk <guptarahul70322@gmail.com>
2025-02-23 10:43:49 +0530

    sample form

--- src/components/PracticePage/PracticePage.tsx
+++ src/components/PracticePage/PracticePage.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import SampleForm from "../Sample/SampleForm";
 import SampleImageUpload from "../Sample/SampleImageUpload";
 
 interface PracticePageProps {}
@@ -9,6 +10,7 @@ const PracticePage: React.FC<PracticePageProps> = ({}) => {
       <Link to={`/personas/edit/ba4626c9-c385-48e9-b5f6-b80bcc8aefd9`}>
         edit persona
       </Link>
+      <SampleForm />
     </div>
   );
 };
