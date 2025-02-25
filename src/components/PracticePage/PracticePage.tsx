29c61f90b46bfaee0964ae56144f5eee9db8d452
rgsk <guptarahul70322@gmail.com>
2025-02-25 22:01:40 +0530

    pdf reader

--- src/components/PracticePage/PracticePage.tsx
+++ src/components/PracticePage/PracticePage.tsx
@@ -1,33 +1,17 @@
-import { Link } from "react-router-dom";
-import useJsonDataKeysLike from "~/hooks/useJsonDataKeysLike";
-import { openaiIdPlaceholder, uuidPlaceholder } from "~/lib/constants";
-import jsonDataService from "~/services/jsonDataService";
-import SampleImageUpload from "../Sample/SampleImageUpload";
-import { Button } from "../ui/button";
+import PDFReader from "../Shared/PDFReader/PDFReader";
 
 interface PracticePageProps {}
 const PracticePage: React.FC<PracticePageProps> = ({}) => {
-  const { data } = useJsonDataKeysLike(
-    `assistants/${openaiIdPlaceholder}/personas/${uuidPlaceholder}/conversations/${uuidPlaceholder}`
-  );
-  console.log({ data });
   return (
     <div className="p-[100px]">
-      <Link to={`/personas/edit/ba4626c9-c385-48e9-b5f6-b80bcc8aefd9`}>
-        edit persona
-      </Link>
-      <SampleImageUpload />
-      <Button
-        onClick={async () => {
-          await jsonDataService.deleteKeysLike({
-            key: `assistants/${openaiIdPlaceholder}/personas/${uuidPlaceholder}/conversations/${uuidPlaceholder}`,
-          });
-        }}
-      >
-        Delete
-      </Button>
+      <div className="w-[50%] border border-red-400">
+        <PDFReader
+          pdfUrl={
+            "https://c08a1eeb-cb81-4c3c-9a11-f616ffd8e042.s3.us-east-1.amazonaws.com/9936232d-abbb-4745-9fd8-fb4291a27392.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIATG6MGJ76MFBVY4PW%2F20250225%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250225T162703Z&X-Amz-Expires=86400&X-Amz-Signature=826477ca6f453111d89896084c23c4cd117f77c9ebda43fceeee6e13090bb0b1&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject"
+          }
+        />
+      </div>
     </div>
   );
 };
 export default PracticePage;
-// https://c08a1eeb-cb81-4c3c-9a11-f616ffd8e042.s3.us-east-1.amazonaws.com/d4cf21cf-f303-4aec-9a38-3fb472433122.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIATG6MGJ76MFBVY4PW%2F20250223%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250223T122838Z&X-Amz-Expires=86400&X-Amz-Signature=0ae99dde42e46ea85ac4e1ec8c04ebe28b2c8b29234811611d7c2e041464c01d&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject
---
+++ src/components/Shared/PDFReader/PDFReader.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import useMeasure from "react-use-measure";
+import { LoadingSpinner } from "../LoadingSpinner";
+const sampleS3Link =
+  "https://c08a1eeb-cb81-4c3c-9a11-f616ffd8e042.s3.us-east-1.amazonaws.com/ddd9207b-333c-44c1-8393-b3cc5536f1c9.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIATG6MGJ76MFBVY4PW%2F20250225%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250225T155354Z&X-Amz-Expires=86400&X-Amz-Signature=16c32fc64549278b015309ade4230d185842c8e400139d07bd6670c9d1a78ccb&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject";
+const sampleLink2 =
+  "https://d2bps9p1kiy4ka.cloudfront.net/5eb393ee95fab7468a79d189/3f3577e6-d8ef-4d91-86b6-f6a6da63fbc9.pdf";
+const sampleLink3 =
+  "https://c08a1eeb-cb81-4c3c-9a11-f616ffd8e042.s3.us-east-1.amazonaws.com/9936232d-abbb-4745-9fd8-fb4291a27392.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIATG6MGJ76MFBVY4PW%2F20250225%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250225T162703Z&X-Amz-Expires=86400&X-Amz-Signature=826477ca6f453111d89896084c23c4cd117f77c9ebda43fceeee6e13090bb0b1&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject";
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
+interface PDFReaderProps {
+  pdfUrl: string;
+}
+const PDFReader: React.FC<PDFReaderProps> = ({ pdfUrl }) => {
+  const [numPages, setNumPages] = useState<number>();
+  const loadingElement = () => (
+    <div className="h-[50vh] flex justify-center items-center">
+      <LoadingSpinner />
+    </div>
+  );
+  const [divRef, divBounds] = useMeasure();
+
+  return (
+    <div ref={divRef}>
+      <Document
+        file={pdfUrl}
+        onLoadSuccess={({ numPages }) => {
+          setNumPages(numPages);
+        }}
+        loading={loadingElement}
+      >
+        {Array.from({ length: numPages ?? 0 }, (_, index) => {
+          const page = index + 1;
+          return (
+            <Page
+              width={divBounds.width}
+              key={page}
+              pageNumber={page}
+              renderAnnotationLayer={false}
+              renderTextLayer={false}
+              loading={loadingElement}
+            />
+          );
+        })}
+      </Document>
+    </div>
+  );
+};
+export default PDFReader;
