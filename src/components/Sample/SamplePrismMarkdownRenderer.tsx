ddbf621ff4ec005867b25a3a6d8669c3ba14a35c
rgsk <guptarahul70322@gmail.com>
2025-03-13 16:30:58 +0530

    fix

--- src/components/Sample/SamplePrismMarkdownRenderer.tsx
+++ src/components/Sample/SamplePrismMarkdownRenderer.tsx
@@ -15,72 +15,73 @@ export function SamplePrismMarkdownRenderer({
 }: MarkdownRendererProps) {
   const { copied, copy } = useCopyToClipboard();
   return (
-    <Markdown
-      remarkPlugins={[remarkGfm]}
-      rehypePlugins={[rehypeRaw]}
-      className="messageContent"
-      components={{
-        code({ node, className, children, ...props }: any) {
-          const match = /language-(\w+)/.exec(className || "");
-          const language = match?.[1] ?? "default";
-          const inline = node.position.start.line === node.position.end.line;
-          if (inline) {
+    <div className="messageContent">
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          code({ node, className, children, ...props }: any) {
+            const match = /language-(\w+)/.exec(className || "");
+            const language = match?.[1] ?? "default";
+            const inline = node.position.start.line === node.position.end.line;
+            if (inline) {
+              return (
+                <code
+                  className={cn(
+                    "bg-gray-200 px-[5px] py-[2px] text-red-500 rounded-sm text-sm",
+                    className
+                  )}
+                  {...props}
+                >
+                  {children}
+                </code>
+              );
+            }
             return (
-              <code
-                className={cn(
-                  "bg-gray-200 px-[5px] py-[2px] text-red-500 rounded-sm text-sm",
-                  className
-                )}
-                {...props}
-              >
-                {children}
-              </code>
-            );
-          }
-          return (
-            <div className="relative">
-              <div
-                className={cn(
-                  "absolute top-0 right-0 bg-[#50505a] w-full",
-                  "flex justify-between items-center",
-                  "rounded-tl-[16px] rounded-tr-[16px]",
-                  "px-[16px] py-[8px]"
-                )}
-              >
-                {language === "default" ? (
-                  <span></span>
-                ) : (
-                  <span className="text-white text-xs">{language}</span>
-                )}
-                <button
-                  className="text-white text-xs"
-                  onClick={() => {
-                    copy(children);
+              <div className="relative">
+                <div
+                  className={cn(
+                    "absolute top-0 right-0 bg-[#50505a] w-full",
+                    "flex justify-between items-center",
+                    "rounded-tl-[16px] rounded-tr-[16px]",
+                    "px-[16px] py-[8px]"
+                  )}
+                >
+                  {language === "default" ? (
+                    <span></span>
+                  ) : (
+                    <span className="text-white text-xs">{language}</span>
+                  )}
+                  <button
+                    className="text-white text-xs"
+                    onClick={() => {
+                      copy(children);
+                    }}
+                  >
+                    {copied ? "Copied!" : "Copy"}
+                  </button>
+                </div>
+                {/* @ts-ignore */}
+                <SyntaxHighlighter
+                  style={vscDarkPlus}
+                  PreTag="div"
+                  language={language}
+                  {...props}
+                  customStyle={{
+                    borderRadius: "16px",
+                    padding: "16px",
+                    paddingTop: "50px",
                   }}
                 >
-                  {copied ? "Copied!" : "Copy"}
-                </button>
+                  {children}
+                </SyntaxHighlighter>
               </div>
-              {/* @ts-ignore */}
-              <SyntaxHighlighter
-                style={vscDarkPlus}
-                PreTag="div"
-                language={language}
-                {...props}
-                customStyle={{
-                  borderRadius: "16px",
-                  padding: "16px",
-                  paddingTop: "50px",
-                }}
-              >
-                {children}
-              </SyntaxHighlighter>
-            </div>
-          );
-        },
-      }}
-    >
-      {markdown}
-    </Markdown>
+            );
+          },
+        }}
+      >
+        {markdown}
+      </Markdown>
+    </div>
   );
 }
