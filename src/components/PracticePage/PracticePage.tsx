d1ba4ce1b096c4a8acbed12f889ea6d7c04dab45
rgsk <guptarahul70322@gmail.com>
2025-03-16 20:52:42 +0530

    math quill

--- src/components/PracticePage/PracticePage.tsx
+++ src/components/PracticePage/PracticePage.tsx
@@ -1,68 +1,50 @@
-import MermaidChart from "../ChatPage/Children/MermaidChart";
-
 interface PracticePageProps {}
 const PracticePage: React.FC<PracticePageProps> = ({}) => {
   return (
     <div>
-      <MermaidChart chart={chartDef} />
+      <p>hii</p>
+      <App />
     </div>
   );
 };
 export default PracticePage;
 
-const chartDef = `
-graph TD;
-    A[Total Expenses: 9434]
-    B[Amount Given: 8000]
-    C[Reimbursement: 1434]
-    D[Amount belonging to Lal and Aksh: 200]
-    E[Amount due to Rahul Gupta: 1234]
+import React, { useState } from "react";
+import { addStyles, EditableMathField, StaticMathField } from "react-mathquill";
+import { Button } from "../ui/button";
 
-    A --> B
-    A --> C
-    C --> D
-    C --> E
+// inserts the required css to the <head> block.
+// you can skip this, if you want to do that by yourself.
+addStyles();
 
-    subgraph Expenses Breakdown
-        F[Uber Trip - Noida to Bhiwadi: 2590]
-        G[Tolls to Driver: 780]
-        H[Pizza Breakfast: 291]
-        I[Lunch in Expo: 400]
-        J[Water Bottles: 40]
-        K[Tea: 40]
-        L[Auto Rikshaw Expo to Hotel: 100]
-        M[Boiled Eggs: 60]
-        N[Egg Rolls: 150]
-        O[Hotel: 1211]
-        P[Soap: 40]
-        Q[Breakfast Sunday Morning: 251]
-        R[Snacks: 130]
-        S[Auto: 100]
-        T[Lunch: 150]
-        U[Water Bottle: 20]
-        V[Tea: 40]
-        W[Taxi to Noida: 3000]
-        X[Ride to home: 41]
-    end
+const EditableMathExample = () => {
+  const [latex, setLatex] = useState("\\frac{1}{\\sqrt{2}}\\cdot 2");
 
-    A --> F
-    A --> G
-    A --> H
-    A --> I
-    A --> J
-    A --> K
-    A --> L
-    A --> M
-    A --> N
-    A --> O
-    A --> P
-    A --> Q
-    A --> R
-    A --> S
-    A --> T
-    A --> U
-    A --> V
-    A --> W
-    A --> X
+  return (
+    <div>
+      <EditableMathField
+        latex={latex}
+        onChange={(mathField) => {
+          setLatex(mathField.latex());
+        }}
+      />
+      <p>{latex}</p>
+      <StaticMathField>{latex}</StaticMathField>
+      <Button>Execute</Button>
+    </div>
+  );
+};
 
-`;
+const StaticMathExample = () => {
+  return <StaticMathField>{"\\frac{1}{\\sqrt{2}}\\cdot 2"}</StaticMathField>;
+};
+
+const App = () => (
+  <div>
+    <h2>Editable Math Field</h2>
+    <EditableMathExample />
+    <br />
+    <h2>Static Math Field</h2>
+    <StaticMathExample />
+  </div>
+);
