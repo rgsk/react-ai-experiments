965568ba071f500998aa1e55403e4df9c237e382
rgsk <guptarahul70322@gmail.com>
2025-04-05 11:45:22 +0530

    dialog wrapper mobile

--- src/components/ChatPage/Children/SearchDialog.tsx
+++ src/components/ChatPage/Children/SearchDialog.tsx
@@ -6,11 +6,11 @@ import { Input } from "../../ui/input";
 
 import { format } from "date-fns";
 import { Messages2 } from "iconsax-react";
-import { Search } from "lucide-react";
+import { Search, X } from "lucide-react";
 import * as React from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import DialogWrapper from "~/components/Shared/DialogWrapper";
 import { Button } from "~/components/ui/button";
-import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
 import useDebounce from "~/hooks/useDebounce";
 import useGlobalContext from "~/hooks/useGlobalContext";
 import { Chat } from "~/lib/typesJsonData";
@@ -60,121 +60,132 @@ export function SearchDialog() {
   }, [open]);
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
+    <DialogWrapper
+      triggerComponent={
         <Button className="" variant="outline" size="icon">
           <Search />
         </Button>
-      </DialogTrigger>
-      <DialogContent className="min-w-[650px] p-0 gap-0 bg-white dark:bg-gray-900 text-black dark:text-white border-gray-300 dark:border-gray-700">
-        <div className="flex items-center border-b border-gray-300 dark:border-gray-700 px-4 py-2">
-          <Search className="h-4 w-4 mr-2 text-gray-500 dark:text-gray-400" />
-          <Input
-            ref={inputRef}
-            value={_searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            placeholder="Search..."
-            className="border-0 pr-10 shadow-none outline-none bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 text-black dark:text-white placeholder:text-gray-400 dark:placeholder:text-gray-500"
-          />
-        </div>
-        <div className="h-[360px] overflow-y-auto">
-          {messagesJsonDataEntries.length > 0 ? (
-            <>
-              {messagesJsonDataEntries.map((d) => {
-                const messages = d.value;
-                const matchingMessage = messages.find(
-                  (m) =>
-                    ["user", "assistant"].includes(m.role) &&
-                    typeof m.content === "string" &&
-                    m.content.toLowerCase().includes(searchQuery.toLowerCase())
-                );
+      }
+      open={open}
+      setOpen={setOpen}
+    >
+      <div className="flex items-center border-b border-gray-300 dark:border-gray-700 px-4 py-2">
+        <Search className="h-5 w-5 mr-2 text-gray-500 dark:text-gray-400" />
+        <Input
+          ref={inputRef}
+          value={_searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search..."
+          className="border-0 pr-10 shadow-none outline-none bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 text-black dark:text-white placeholder:text-gray-400 dark:placeholder:text-gray-500"
+        />
 
-                let content = matchingMessage?.content as string | undefined;
-                let snippet = "";
-                if (content) {
-                  const index = content
-                    .toLowerCase()
-                    .indexOf(searchQuery.toLowerCase());
+        <Button
+          size="icon"
+          variant="ghost"
+          onClick={() => {
+            setOpen(false);
+          }}
+        >
+          <X />
+        </Button>
+      </div>
+      <div className="h-[360px] overflow-y-auto">
+        {messagesJsonDataEntries.length > 0 ? (
+          <>
+            {messagesJsonDataEntries.map((d) => {
+              const messages = d.value;
+              const matchingMessage = messages.find(
+                (m) =>
+                  ["user", "assistant"].includes(m.role) &&
+                  typeof m.content === "string" &&
+                  m.content.toLowerCase().includes(searchQuery.toLowerCase())
+              );
 
-                  if (index !== -1) {
-                    const beforeMatch = content.substring(0, index);
-                    const match = content.substring(
-                      index,
-                      index + searchQuery.length
-                    );
-                    const afterMatch = content.substring(
-                      index + searchQuery.length
-                    );
-                    content = `${beforeMatch}<b>${match}</b>${afterMatch}`;
-                  }
-                  const snippetLength = 30; // Adjust this to control snippet size
+              let content = matchingMessage?.content as string | undefined;
+              let snippet = "";
+              if (content) {
+                const index = content
+                  .toLowerCase()
+                  .indexOf(searchQuery.toLowerCase());
 
-                  snippet = content;
-                  if (index !== -1) {
-                    const start = Math.max(0, index - snippetLength / 2);
-
-                    snippet = (start > 0 ? "..." : "") + content.slice(start);
-                  }
+                if (index !== -1) {
+                  const beforeMatch = content.substring(0, index);
+                  const match = content.substring(
+                    index,
+                    index + searchQuery.length
+                  );
+                  const afterMatch = content.substring(
+                    index + searchQuery.length
+                  );
+                  content = `${beforeMatch}<b>${match}</b>${afterMatch}`;
                 }
+                const snippetLength = 30; // Adjust this to control snippet size
 
-                const match = d.key.match(/\/chats\/([^/]+)\/messages/);
+                snippet = content;
+                if (index !== -1) {
+                  const start = Math.max(0, index - snippetLength / 2);
 
-                const chatId = match![1];
-                const chat = idToChat[chatId];
-                return (
-                  <div key={d.id}>
-                    <div
-                      className="px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer flex justify-between items-center gap-2"
-                      onClick={() => {
-                        setOpen(false);
-                        setIsFirstHistoryRender(true);
-                        if (personaId) {
-                          if (matchingMessage) {
-                            navigate(
-                              `/chat/${chatId}?personaId=${personaId}#message-${matchingMessage.id}`
-                            );
-                          } else {
-                            navigate(`/chat/${chatId}?personaId=${personaId}`);
-                          }
+                  snippet = (start > 0 ? "..." : "") + content.slice(start);
+                }
+              }
+
+              const match = d.key.match(/\/chats\/([^/]+)\/messages/);
+
+              const chatId = match![1];
+              const chat = idToChat[chatId];
+              return (
+                <div key={d.id}>
+                  <div
+                    className="px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer flex justify-between items-center gap-2"
+                    onClick={() => {
+                      setOpen(false);
+                      setIsFirstHistoryRender(true);
+                      if (personaId) {
+                        if (matchingMessage) {
+                          navigate(
+                            `/chat/${chatId}?personaId=${personaId}#message-${matchingMessage.id}`
+                          );
                         } else {
-                          if (matchingMessage) {
-                            navigate(
-                              `/chat/${chatId}#message-${matchingMessage.id}`
-                            );
-                          } else {
-                            navigate(`/chat/${chatId}`);
-                          }
+                          navigate(`/chat/${chatId}?personaId=${personaId}`);
                         }
-                      }}
-                    >
-                      <Messages2 className="min-w-[16px] w-[16px] text-gray-600 dark:text-gray-400" />
-                      <div className="flex flex-col">
-                        <h4 className="text-sm font-medium">{chat?.title}</h4>
-                        <p
-                          className="text-sm text-gray-600 dark:text-gray-400 line-clamp-1"
-                          dangerouslySetInnerHTML={{ __html: snippet }}
-                        ></p>
-                      </div>
-                      <div className="flex-1"></div>
-                      <p className="text-xs text-gray-400 dark:text-gray-500 mt-1">
-                        {format(chat?.createdAt ?? new Date(), "dd/MM/yy")}
-                      </p>
+                      } else {
+                        if (matchingMessage) {
+                          navigate(
+                            `/chat/${chatId}#message-${matchingMessage.id}`
+                          );
+                        } else {
+                          navigate(`/chat/${chatId}`);
+                        }
+                      }
+                    }}
+                  >
+                    <Messages2 className="min-w-[16px] w-[16px] text-gray-600 dark:text-gray-400" />
+                    <div className="flex flex-col">
+                      <h4 className="text-sm font-medium">{chat?.title}</h4>
+                      <p
+                        className="text-sm text-gray-600 dark:text-gray-400 line-clamp-1"
+                        dangerouslySetInnerHTML={{ __html: snippet }}
+                      ></p>
                     </div>
+                    <div className="flex-1"></div>
+                    <p className="text-xs text-gray-400 dark:text-gray-500 mt-1">
+                      {format(chat?.createdAt ?? new Date(), "dd/MM/yy")}
+                    </p>
                   </div>
-                );
-              })}
-            </>
-          ) : searchQuery ? (
-            <>
-              <div className="px-4 py-8 text-center text-gray-500 dark:text-gray-400">
-                No results found for "{searchQuery}"
-              </div>
-            </>
-          ) : (
-            <></>
-          )}
-        </div>
-      </DialogContent>
-    </Dialog>
+                </div>
+              );
+            })}
+          </>
+        ) : searchQuery ? (
+          <>
+            <div className="px-4 py-8 text-center text-gray-500 dark:text-gray-400">
+              No results found for "{searchQuery}"
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    </DialogWrapper>
   );
 }
---
+++ src/components/Shared/DialogWrapper.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import * as React from "react";
+import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
+import useBreakpoints from "~/hooks/useBreakpoints";
+
+interface DialogWrapperProps {
+  children: any;
+  triggerComponent: any;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const DialogWrapper: React.FC<DialogWrapperProps> = ({
+  triggerComponent,
+  children,
+  open,
+  setOpen,
+}) => {
+  const { md } = useBreakpoints();
+  return (
+    <>
+      {md ? (
+        <>
+          <DesktopDialogWrapper
+            triggerComponent={triggerComponent}
+            open={open}
+            setOpen={setOpen}
+          >
+            {children}
+          </DesktopDialogWrapper>
+        </>
+      ) : (
+        <>
+          <MobileDialogWrapper
+            triggerComponent={triggerComponent}
+            open={open}
+            setOpen={setOpen}
+          >
+            {children}
+          </MobileDialogWrapper>
+        </>
+      )}
+    </>
+  );
+};
+export default DialogWrapper;
+
+interface DesktopDialogWrapperProps {
+  children: any;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  triggerComponent: any;
+}
+const DesktopDialogWrapper: React.FC<DesktopDialogWrapperProps> = ({
+  children,
+  open,
+  setOpen,
+  triggerComponent,
+}) => {
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{triggerComponent}</DialogTrigger>
+      <DialogContent
+        hideCloseIcon
+        className="min-w-[650px] p-0 gap-0 bg-white dark:bg-gray-900 text-black dark:text-white border-gray-300 dark:border-gray-700"
+      >
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+interface MobileDialogWrapperProps {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  children: any;
+  triggerComponent: any;
+}
+const MobileDialogWrapper: React.FC<MobileDialogWrapperProps> = ({
+  children,
+  open,
+  setOpen,
+  triggerComponent,
+}) => {
+  return (
+    <div>
+      <div
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        {triggerComponent}
+      </div>
+      {open && (
+        <div className="fixed top-0 left-0 w-full h-full z-50 bg-white">
+          {children}
+        </div>
+      )}
+    </div>
+  );
+};
