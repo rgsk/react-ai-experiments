f3087ef8e59cbdc83f7e96abfb87a8f9354ebba0
rgsk <guptarahul70322@gmail.com>
2025-04-05 17:54:24 +0530

    fixed

--- src/components/SharedChatPage/SharedChatPage.tsx
+++ src/components/SharedChatPage/SharedChatPage.tsx
@@ -1,5 +1,6 @@
-import { Check, Download, Home, Share } from "lucide-react";
-import { Link, useParams } from "react-router-dom";
+import { Check, Copy, Download } from "lucide-react";
+import { useParams } from "react-router-dom";
+import useBreakpoints from "~/hooks/useBreakpoints";
 import useCopyToClipboard from "~/hooks/useCopyToClipboard";
 import useJsonData from "~/hooks/useJsonData";
 import { createMarkdownContent } from "~/lib/chatUtils";
@@ -8,7 +9,6 @@ import { downloadContentFile } from "~/lib/utils";
 import RenderMessages from "../ChatPage/Children/RenderMessages/RenderMessages";
 import CentralLoader from "../Shared/CentralLoader";
 import Container from "../Shared/Container";
-import { ModeToggle } from "../Shared/ModeToggle";
 import { Button } from "../ui/button";
 
 interface SharedChatPageProps {}
@@ -19,7 +19,7 @@ const SharedChatPage: React.FC<SharedChatPageProps> = ({}) => {
   const [sharedChat] = useJsonData<SharedChat>(
     `admin/public/sharedChats/${sharedChatId}`
   );
-
+  const { md } = useBreakpoints();
   const exportChat = () => {
     if (!sharedChat) return;
     const markdown = createMarkdownContent(sharedChat.messages);
@@ -34,31 +34,27 @@ const SharedChatPage: React.FC<SharedChatPageProps> = ({}) => {
   }
 
   return (
-    <div className="h-screen flex flex-col">
-      <div className="border-b border-b-input p-4 flex justify-between items-center">
-        <span className="flex gap-2">
-          <Link to="/">
-            <Button variant="outline" size="icon">
-              <Home />
+    <div className="h-full flex flex-col">
+      <div className="border-b border-b-input p-4 grid md:grid-cols-3 items-center">
+        <div></div>
+        <div className="text-center">{sharedChat.chat.title}</div>
+        {md && (
+          <div className="flex gap-2 justify-end">
+            <Button
+              variant="outline"
+              onClick={() => {
+                copy(window.location.href);
+              }}
+            >
+              {copied ? <Check /> : <Copy />}
+              <span>Copy Link</span>
             </Button>
-          </Link>
-        </span>
-        <span>{sharedChat.chat.title}</span>
-        <span className="flex gap-2">
-          <Button
-            variant="outline"
-            onClick={() => {
-              copy(window.location.href);
-            }}
-            size="icon"
-          >
-            {copied ? <Check /> : <Share />}
-          </Button>
-          <Button variant="outline" onClick={exportChat} size="icon">
-            <Download />
-          </Button>
-          <ModeToggle />
-        </span>
+            <Button variant="outline" onClick={exportChat}>
+              <Download />
+              <span>Export</span>
+            </Button>
+          </div>
+        )}
       </div>
       <div className="flex-1 overflow-auto">
         <Container>
