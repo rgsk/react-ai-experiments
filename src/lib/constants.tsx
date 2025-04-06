3cd2f300ff2934178ce7b833d28d9690b09de615
rgsk <guptarahul70322@gmail.com>
2025-04-06 10:14:36 +0530

    fix

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -1,10 +1,3 @@
-import { DocumentText1, Gallery } from "iconsax-react";
-
-export const fileIcons = {
-  document: <DocumentText1 size={16} />,
-  image: <Gallery size={16} />,
-};
-
 export const uuidPlaceholder = "________-____-____-____-____________";
 export const openaiIdPlaceholder = "_____________________________";
 export const defaultModel = "openai/gpt-4o";
