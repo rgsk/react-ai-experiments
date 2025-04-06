4996f90eec30b56145de2da1de2816ecd02d6300
rgsk <guptarahul70322@gmail.com>
2025-04-06 12:44:09 +0530

    fixes

--- src/components/ChatPage/ChatPage.tsx
+++ src/components/ChatPage/ChatPage.tsx
@@ -923,14 +923,27 @@ const ChatPage: React.FC<ChatPageProps> = ({}) => {
       }
       if (modelOptions[model].toolsSupport) {
         additionalInstructions.push(html`
-          when running the executeTool code with python for data analysis. don't
-          ever load entire csv file content into your context (by either
-          fetching csv content via url or printing the entire file content),
-          otherwise your context will get bloated as csv data files are usually
-          very large, nor attempt to type out the data line by line, you are
-          given a url to csv file, use that url to fetch the "head" (small
-          portions of lines), if some error occurs fetch a different portion of
-          the csv file, but never the entire content
+          <pre>
+            when running the executeTool code with python for data analysis.
+            don't ever load entire csv file content into your context (by either
+            fetching csv content via url or printing the entire file content),
+
+            otherwise your context will get bloated as csv data files are usually
+            very large
+            
+            nor attempt to type out the data line by line, you are
+            given a url to csv file, use that url to fetch the "head" (small
+            portions of lines), if some error occurs fetch a different portion of
+            the csv file, but never the entire content.
+
+            exception: you are allowed to fetch the contents of head.csv file, by running getUrlContent tool
+            
+            when you given with a csv url, the first step should be view it's head to understand it, 
+            don't fetch entire csv, only fetch the head, in case parsing head fails, 
+            it implies that csv file might have unusual structure, 
+            in that case you are allowed to get entire csv contents.
+            </pre
+          >
         `);
       }
     }
--- src/lib/clientTools.ts
+++ src/lib/clientTools.ts
@@ -26,15 +26,34 @@ const clientTools: Tool[] = [
                 you also have the ability to generate urls for csv files and
                 graphs
                 <span>
-                  # for eg for csv file you can do below import pandas as pd
-                  data = pd.DataFrame({'x': [1, 2, 3], 'y': [2, 4, 6]})
-                  data.to_csv("filename.csv") # or data.head() # data will be
-                  saved to head.csv
+                  # for eg for csv file you can do below
+                  <pre>
+                    import pandas as pd
+                    data = pd.DataFrame({'x': [1, 2, 3], 'y': [2, 4, 6]})
+                    data.to_csv("filename.csv") 
+                    # or 
+                    data.head() # data will be saved to head.csv
+                  </pre
+                  >
                 </span>
                 <span>
                   when you have the file url, use code like below to fetch it's
                   contents
-                  pd.read_csv('https://pubbuckrah.s3.us-east-1.amazonaws.com/91bff1cc-6a88-4c4f-9af0-097f0428d8cc/sample_linear_regression.csv')
+                  <pre>
+                    pd.read_csv('https://pubbuckrah.s3.us-east-1.amazonaws.com/91bff1cc-6a88-4c4f-9af0-097f0428d8cc/sample_linear_regression.csv')
+                 </pre
+                  >
+
+                  <important>
+                    use only above method to fetch csv content, don't run code
+                    like below (because requests module is not available)
+                    <pre>
+                      response = requests.get(url) 
+                      with open('head.csv', 'wb') as file: 
+                          file.write(response.content)
+                      </pre
+                    >
+                  </important>
                 </span>
                 <span>
                   in most cases you are given both csv url and csv content (csv
@@ -43,10 +62,26 @@ const clientTools: Tool[] = [
                   so that you can respond quickly</span
                 >
                 <span>
-                  # for generating graph use matplotlib like below import
-                  matplotlib.pyplot as plt x = [1, 2, 3, 4, 5] y = [10, 20, 25,
-                  30, 50] plt.plot(x, y, marker='o', linestyle='-', color='b',
-                  label='Line 1') plt.legend() plt.show()
+                  # for generating graph use matplotlib like below (you have
+                  only matplotlib in this environment, no seaborn)
+                  <pre>
+                    import matplotlib.pyplot as plt 
+                    x = [1, 2, 3, 4, 5] 
+                    y = [10, 20, 25, 30, 50] 
+                    plt.plot(x, y, marker='o', linestyle='-', color='b', label='Line 1') 
+                    plt.legend() 
+                    plt.show()
+                  </pre
+                  >
+                  <important>
+                    don't run code like below, just running plt.show() is
+                    sufficient
+                    <pre>
+                    # Save to file
+                    plt.savefig('/mnt/data/housing_graphs_corrected.png')
+                  </pre
+                    >
+                  </important>
                 </span>
               </note> `,
           },
