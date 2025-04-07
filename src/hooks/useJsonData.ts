378517b597eb802009d8d7de6703291693d35d02
rgsk <guptarahul70322@gmail.com>
2025-04-07 12:08:28 +0530

    fix

--- src/hooks/useJsonData.ts
+++ src/hooks/useJsonData.ts
@@ -1,5 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-import { firebaseAuth } from "~/lib/firebaseApp";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import jsonDataService from "~/services/jsonDataService";
 import usePrevious from "./usePrevious";
 
@@ -28,10 +27,45 @@ function useJsonData<T>(
   const previousKey = usePrevious(key);
   const previousKeyRef = useRef(previousKey);
   previousKeyRef.current = previousKey;
-  const lastFetchedValueRef = useRef<T | undefined>();
+  const lastFetchedValueRef = useRef<T | undefined>(undefined);
   const migrationRef = useRef(migration);
   migrationRef.current = migration;
-
+  const setSharedState = useCallback(
+    (
+      valueOrFunction:
+        | (T | undefined)
+        | ((prev: T | undefined) => T | undefined),
+      mandatorySetKey = false
+    ) => {
+      // Allow value to be a function so we have same API as useState
+      const newState =
+        valueOrFunction instanceof Function
+          ? valueOrFunction(localValueRef.current)
+          : valueOrFunction;
+      setUpdating(true);
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+      if (mandatorySetKey) {
+        (async () => {
+          await jsonDataService.setKey({
+            key: keyRef.current,
+            value: newState,
+          });
+          setUpdating(false);
+        })();
+      } else {
+        timerRef.current = setTimeout(async () => {
+          await jsonDataService.setKey({
+            key: keyRef.current,
+            value: newState,
+          });
+          setUpdating(false);
+        }, 1000);
+      }
+    },
+    []
+  );
   const populateState = useCallback(async () => {
     if (!enabled) {
       setLocalValue(undefined);
@@ -59,7 +93,7 @@ function useJsonData<T>(
       if (value !== undefined) {
         if (migrationRef.current) {
           const migratedValue = migrationRef.current(value);
-          setLocalValue(migratedValue);
+          setSharedState(migratedValue);
         } else {
           setLocalValue(value);
         }
@@ -69,23 +103,17 @@ function useJsonData<T>(
             ? initialValueRef.current()
             : initialValueRef.current;
         if (finalInitialValue !== undefined) {
-          setLocalValue(finalInitialValue);
+          setSharedState(finalInitialValue);
         }
       }
     } catch (err) {
       console.error(err);
     }
     setLoading(false);
-  }, [enabled, key]);
+  }, [enabled, key, setLocalValue, setSharedState]);
 
   useEffect(() => {
     void populateState();
-  }, [populateState]);
-
-  useEffect(() => {
-    return firebaseAuth.onAuthStateChanged(async (user) => {
-      populateState();
-    });
   }, [populateState]);
 
   useEffect(() => {
@@ -100,23 +128,7 @@ function useJsonData<T>(
     };
   }, []);
   const [updating, setUpdating] = useState(false);
-  const timerRef = useRef<NodeJS.Timeout>();
-  useEffect(() => {
-    if (enabled) {
-      if (timerRef.current) {
-        clearTimeout(timerRef.current);
-      }
-      setUpdating(true);
-      const latestValue = localValue;
-      timerRef.current = setTimeout(async () => {
-        await jsonDataService.setKey({
-          key: keyRef.current,
-          value: latestValue,
-        });
-        setUpdating(false);
-      }, 1000);
-    }
-  }, [enabled, localValue]);
+  const timerRef = useRef<NodeJS.Timeout>(undefined);
   const refetch = useCallback(async () => {
     const result = await jsonDataService.getKey<T>({
       key,
@@ -126,15 +138,23 @@ function useJsonData<T>(
       setLocalValue(value);
     }
   }, [key]);
+  const setSharedAndLocalState = useMemo(() => {
+    return callBoth(setLocalValue, setSharedState);
+  }, [setSharedState]);
   return [
     localValue,
-    setLocalValue,
+    setSharedAndLocalState,
     { loading, refetch: refetch, updating },
   ] as const;
 }
 export default useJsonData;
 
-export type SetSharedState<T> = (
-  valueOrFunction: (T | undefined) | ((prev: T | undefined) => T | undefined),
-  mandatorySetKey?: boolean
-) => void;
+function callBoth<T extends any[]>(
+  fn1: (...args: T) => void,
+  fn2: (...args: T) => void
+) {
+  return (...args: T) => {
+    fn1(...args);
+    fn2(...args);
+  };
+}
